bbd0361e2ceca7f020471b074d4bc618a241a09e
Magomed Ilyasov <mkznworkflow@gmail.com>
2021-05-29 18:11:00 +0300

    added username alert

--- utils/users.js
+++ utils/users.js
@@ -10,8 +10,16 @@
  }
 
  const getCurrentUser = (id) => {
-   return users.find(user => user.id == id);
+   return users.find(user => user.id === id);
  }
+
+const userLeave = (id) => {
+  const index = users.findIndex(user => user.id === id)
+
+  if (index !== -1) {
+    return users.splice(index, 1);
+  }
+}
 
 
  module.exports = {
