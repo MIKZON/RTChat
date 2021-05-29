8ba2f6e0a10c8165de925801a42b309a81a021e6
Magomed Ilyasov <mkznworkflow@gmail.com>
2021-05-29 17:12:56 +0300

    added auto-correct input after message

--- public/js/main.js
+++ public/js/main.js
@@ -20,6 +20,9 @@ chatForm.addEventListener('submit', (e) => {
   const msg = e.target.elements.msg.value;
 
   socket.emit('chatMessage', msg);
+
+   e.target.elements.msg.value = '';
+   e.target.elements.msg.focus();
 });
 
 
